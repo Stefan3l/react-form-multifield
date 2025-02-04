aaef37f80d10e361dc7a7dbb320f254f9c1a7e66
Stefanel Mihaila <stefanel.mihaila@yahoo.com>
2025-02-04 21:58:11 +0100

    add button delete

--- src/App.jsx
+++ src/App.jsx
@@ -12,10 +12,6 @@ export default function App() {
     avaible: false,
   };
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-  };
-
   const hanldeFormList = (fieldName, value) => {
     setFormDataCars((curentState) => ({
       ...curentState,
@@ -23,34 +19,64 @@ export default function App() {
     }));
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    setList((currentList) => [...currentList, formDataCars]);
+    setFormDataCars({
+      marca: "",
+      modelo: "",
+      motore: "",
+      avaible: false,
+    });
+  };
+
+  const handleCarList = (itemDeleteCar) => {
+    setList((currentState) =>
+      currentState.filter((item) => item !== itemDeleteCar)
+    );
+  };
+
+  const handleDeleteList = () => {
+    setList([]);
+  };
+
   return (
     <div className="container">
+      <h1>Lista macchine</h1>
       <ul>
-        {list.map((item) => (
+        {list.map((item, index) => (
           <li key={item.marca}>
-            {item.modelo} - {item.motore}
+            {index} - {item.modelo} - {item.motore} ----
+            <strong>{item.avaible ? "disponibile" : "non disponibine"}</strong>
+            <button onClick={() => handleCarList(item)}>🗑️</button>
           </li>
         ))}
       </ul>
+      <button onClick={handleDeleteList}>Cancella lista</button>
       <form onSubmit={handleSubmit}>
         <input
           type="text"
           placeholder="inserisci la marca"
           value={formDataCars.marca}
           onChange={(e) => hanldeFormList("marca", e.target.value)}
+          required
         ></input>
         <input
           type="text"
           placeholder="inserisci modelo"
           value={formDataCars.modelo}
           onChange={(e) => hanldeFormList("modelo", e.target.value)}
+          required
         ></input>
         <label>Choose a motore:</label>
         <select
           name="motore"
           value={formDataCars.motore}
-          nChange={(e) => hanldeFormList("motore", e.target.value)}
+          onChange={(e) => hanldeFormList("motore", e.target.value)}
+          required
         >
+          <option value="">None</option>
           <option value="1.4">1.4</option>
           <option value="1.6">1.6</option>
           <option value="1.8">1.8</option>
@@ -59,7 +85,7 @@ export default function App() {
         <label htmlFor="">Validazione</label>
         <input
           type="checkbox"
-          nChange={(e) => hanldeFormList("modelo", e.target.checked)}
+          onChange={(e) => hanldeFormList("avaible", e.target.checked)}
         ></input>
         <button type="submit">Invia</button>
       </form>
